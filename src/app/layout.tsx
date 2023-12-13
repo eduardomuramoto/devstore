90b99a5598d49593082a29dbab446f4f38100c95
Eduardo Muramoto <dudmuramoto@gmail.com>
2023-12-13 15:03:02 -0300

    add basic homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({
-   subsets: ['latin'], 
-   variable: '--font-inter'
-   })
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
-  title: 'DevStore',
-}
+  title: "DevStore",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html className={inter.variable} lang="en">
-      <body className='bg-zinc-950 text-zinc-50 antialiased'>{children}</body>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
-  )
+  );
 }
