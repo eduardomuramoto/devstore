0bc03c4f918dfb7ccad4348f7847e78f948efbf1
Eduardo Muramoto <dudmuramoto@gmail.com>
2023-12-14 14:36:23 -0300

    search page

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,7 +1,69 @@
-export default function SearchPage() {
+import { api } from '@/data/api'
+import { Product } from '@/data/types/products'
+import Link from 'next/link'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+async function getSearchedProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // re-cache every 1 hour
+    },
+  })
+  const products = await response.json()
+
+  return products
+}
+
+export default async function SearchPage({ searchParams }: SearchProps) {
+  const { q: query } = searchParams
+
+  if (!query) {
+    redirect('/')
+  }
+
+  const products = await getSearchedProducts(query)
   return (
-    <div>
-      <h1>Search Page</h1>
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Search results for: <span className="font-semibold">{query}</span>
+      </p>
+      <div className="grid grid-cols-3 gap-6">
+        {products.map((product) => {
+          return (
+            <Link
+              key={product.id}
+              href={`/product/${product.slug}`}
+              className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+            >
+              <Image
+                className="group-hover:scale-105 transition-transform duration-500"
+                src={product.image}
+                width={480}
+                height={480}
+                quality={100}
+                alt=""
+              />
+              <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                <span className="text-sm truncate">{product.title}</span>
+                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                  {product.price.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    maximumFractionDigits: 0,
+                  })}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
     </div>
   )
 }
