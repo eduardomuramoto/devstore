214604190c76e07752955470d436c286dd34c5ed
Eduardo Muramoto <dudmuramoto@gmail.com>
2023-12-14 14:53:34 -0300

    search loading

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,27 @@
-export default function HomeLoading() {
-  return <p>Loading Search....</p>
+'use client'
+
+import { Skeleton } from '@/components/skeleton'
+import { useSearchParams } from 'next/navigation'
+
+export default function SearchLoading() {
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Search results for: <span className="font-semibold">{query}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-6">
+        <Skeleton className="h-[380px]"></Skeleton>
+        <Skeleton className="h-[380px]"></Skeleton>
+        <Skeleton className="h-[380px]"></Skeleton>
+        <Skeleton className="h-[380px]"></Skeleton>
+        <Skeleton className="h-[380px]"></Skeleton>
+        <Skeleton className="h-[380px]"></Skeleton>
+      </div>
+    </div>
+  )
 }
